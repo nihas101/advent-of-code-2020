bfa9ccf2a2564ed4e4b42fce6475d37b749d4176
Nikita Hasert <nikitahasert@gmail.com>
2020-12-04 22:51:43 +0100

    Day4: Use spec

--- src/advent_of_code_2020/day04.clj
+++ src/advent_of_code_2020/day04.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2020.day04
   (:require
-   [clojure.string :as string]))
+   [clojure.string :as string]
+   [clojure.spec.alpha :as s]))
 
 (defn- parse-passport [passport]
   (reduce (fn [p [k v]] (assoc p (keyword k) v))
@@ -23,31 +24,29 @@
                         (string/split passport-batch #"\n\n|\r\n\r\n"))]
      (count (filter valid-passport-1? passports)))))
 
-(defn valid-height? [hgt]
-  (cond
-    ; Inches
-    (string/ends-with? hgt "in")
-    (<= 59 (read-string (subs hgt 0 (- (count hgt) 2))) 76)
-    ; Centimeters
-    (string/ends-with? hgt "cm")
-    (<= 150 (read-string (subs hgt 0 (- (count hgt) 2))) 193)
-    :else false))
-
 (defonce ^:private pid-regex
   (re-pattern
    (string/join "|"
                 (mapv (fn [[n m]] (str "0{" n "}[0-9]{" m "}"))
                       [[0 9] [1 8] [2 7] [3 6] [4 5] [5 4] [6 3] [7 2] [8 1]]))))
 
-(defn valid-passport-2? [{:keys [byr iyr eyr hgt hcl ecl pid] :as passport}]
-  (and (empty? (remove (set (keys passport)) required-keys))
-       (and byr (<= 1920 (read-string byr) 2002))
-       (and iyr (<= 2010 (read-string iyr) 2020))
-       (and eyr (<= 2020 (read-string eyr) 2030))
-       (and hgt (valid-height? hgt))
-       (and hcl (= (re-matches #"#[0-9a-f]{6}" hcl) hcl))
-       (and ecl (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} ecl))
-       (and pid (= (re-matches pid-regex pid) pid))))
+(s/def ::byr (s/and string? #(<= 1920 (Integer/parseInt %) 2002)))
+(s/def ::iyr (s/and string? #(<= 2010 (Integer/parseInt %) 2020)))
+(s/def ::eyr (s/and string? #(<= 2020 (Integer/parseInt %) 2030)))
+(s/def ::hgt (s/and string?
+                    (s/or :in (s/and #(string/ends-with? % "in")
+                                     #(<= 59 (Integer/parseInt (subs % 0 (- (count %) 2))) 76))
+                          :cm (s/and #(string/ends-with? % "cm")
+                                     #(<= 150 (Integer/parseInt (subs % 0 (- (count %) 2))) 193)))))
+(s/def ::hcl (s/and string?
+                    #(= (re-matches #"#[0-9a-f]{6}" %) %)))
+(s/def ::ecl #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+(s/def ::pid (s/and string?
+                    #(= (re-matches pid-regex %) %)))
+(s/def ::cid any?)
+
+(s/def ::passport (s/keys :req-un [::byr ::iyr ::eyr ::hgt ::hcl ::ecl ::pid]
+                          :opt-un [::cid]))
 
 (defn day4-2
   "https://adventofcode.com/2020/day/4"
@@ -55,5 +54,6 @@
    (day4-2 (slurp "resources/passports.txt")))
   ([passport-batch]
    (let [passports (map parse-passport
+                        ;; Split on empty lines
                         (string/split passport-batch #"\n\n|\r\n\r\n"))]
-     (count (filter valid-passport-2? passports)))))
+     (count (filter (partial s/valid? ::passport) passports)))))
