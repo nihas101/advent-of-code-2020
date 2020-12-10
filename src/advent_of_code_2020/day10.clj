208943c5e111121bbd3fdc5807b7a3b7f9d026b0
Nikita Hasert <nikitahasert@gmail.com>
2020-12-10 17:18:25 +0100

    Solve day 10

---
+++ src/advent_of_code_2020/day10.clj
@@ -0,0 +1,34 @@
+(ns advent-of-code-2020.day10
+  (:require
+   [clojure.string :as string]))
+
+(defonce ^:private adapters
+  (map #(Integer/parseInt %)
+       (string/split-lines (slurp "resources/adapters.txt"))))
+
+(defn day10-1
+  ([] (day10-1 adapters))
+  ([adapters]
+   (let [sorted-adapters (sort adapters)]
+     (-> (concat (mapv - sorted-adapters (conj sorted-adapters 0)) [3])
+         frequencies
+         ((juxt #(get % 1) #(get % 3)))
+         ((fn [[a b]] (* ^long a ^long b)))))))
+
+(def adapter-combinations
+  (memoize ;; Cache subresults to speed up calucation
+   (fn [[a _ c & ads :as sorted-adapters]]
+     (cond
+       (or (not c)) (long 1)
+       (<= (- ^long c ^long a) 3) (+ ^long (adapter-combinations (rest sorted-adapters))
+                                     ^long (adapter-combinations (conj ads c a)))
+       :else (recur (rest sorted-adapters))))))
+
+(defn day10-2
+  ([] (day10-2 adapters))
+  ([adapters]
+   (if (< (count adapters) 2)
+     1
+     (let [sorted-adapters (sort adapters)]
+       (adapter-combinations
+        (concat [0] sorted-adapters [(+ ^long (last sorted-adapters) (long 3))]))))))
---
+++ test/advent_of_code_2020/day10_test.clj
@@ -0,0 +1,45 @@
+(ns advent-of-code-2020.day10-test
+  (:require
+   [clojure.test :refer :all]
+   [advent-of-code-2020.day10 :refer :all]))
+
+(defonce ^:private adapters-1 [16 10 15 5 1 11 7 19 6 12 4])
+
+(defonce ^:private adapters-2 [28 33 18 42 31 14 46 20 48 47 24 23 49 45 19
+                               38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3])
+
+(deftest day10-1-example-1-test
+  (testing "Day 10 - Part 1 - Example 1"
+    (is (= 35 (day10-1 adapters-1)))))
+
+(deftest day10-1-example-2-test
+  (testing "Day 10 - Part 1 - Example 2"
+    (is (= 220 (day10-1 adapters-2)))))
+
+(deftest day10-1-test
+  (testing "Day 10 - Part 1"
+    (is (= 1656 (day10-1)))))
+
+(deftest day10-2-example-1-test
+  (testing "Day 10 - Part 2 - Example 1"
+    (is (= 8 (day10-2 adapters-1)))))
+
+(deftest day10-2-example-2-test
+  (testing "Day 10 - Part 2 - Example 2"
+    (is (= 19208 (day10-2 adapters-2)))))
+
+(deftest day10-2-example-3-test
+  (testing "Day 10 - Part 2 - Example 3"
+    (is (= 1 (day10-2 [])))))
+
+(deftest day10-2-example-4-test
+  (testing "Day 10 - Part 2 - Example 4"
+    (is (= 2 (day10-2 [1 2])))))
+
+(deftest day10-2-example-5-test
+  (testing "Day 10 - Part 2 - Example 5"
+    (is (= 4 (day10-2 [1 2 3])))))
+
+(deftest day10-2-test
+  (testing "Day 10 - Part 2"
+    (is (= 56693912375296 (day10-2)))))
