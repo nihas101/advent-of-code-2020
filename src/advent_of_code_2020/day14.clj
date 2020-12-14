7e3ea713a12fbb38a265179d0a376a635a4312f1
Nikita Hasert <nikitahasert@gmail.com>
2020-12-14 18:25:53 +0100

    Day 14: Improve performance

--- src/advent_of_code_2020/day14.clj
+++ src/advent_of_code_2020/day14.clj
@@ -2,21 +2,27 @@
   (:require
    [clojure.string :as string]))
 
-(defmulti read-instruction (fn [instr] (first (string/split instr #"\s+|\[" 2))))
+(defmulti read-instruction (fn [version instr] [version (first (string/split instr #"\s+|\[" 2))]))
 
-(defmethod read-instruction "mem" [instr]
+(defn read-assign [instr]
   (let [[_ sink v] (string/split instr #"(mem\[)|(\]\s+=\s+)")]
-    [:assign (Integer/parseInt sink) (reverse (Integer/toBinaryString
-                                               (Integer/parseInt v)))]))
+    [:assign (Integer/parseInt sink) (Integer/parseInt v)]))
 
-(defmethod read-instruction "mask" [mask]
-  [:mask (reverse (drop 7 mask))])
+(defmethod read-instruction [1 "mem"] [_ instr] (read-assign instr))
 
-(defn- read-program [s]
+(defn- mask [m replacement]
+  (read-string
+   (string/join
+    (reduce (fn [msk bit] (conj msk (replacement bit))) ["2r0"] m))))
+
+(defmethod read-instruction [1 "mask"] [_ msk]
+  [:mask [(mask (drop 7 msk) {\X \0 \0 \0 \1 \1})
+          (mask (drop 7 msk) {\X \1 \0 \0 \1 \1})]])
+
+(defn- read-program [version s]
   (let [program (string/split-lines s)]
-    (eduction (map (fn [instr] (read-instruction instr)))
+    (eduction (map (fn [instr] (read-instruction version instr)))
               program)))
-
 
 (defmulti execute-program (fn [{:keys [version]} [op]] [version op]))
 
@@ -24,50 +30,55 @@
   (assoc mem op new-mask))
 
 (defmethod execute-program [1 :assign] [{:keys [mask] :as mem} [_ sink value]]
-  (assoc-in mem [:mem sink]
-            (mapv (fn [m b] (if (= \X m) b m))
-                  mask (concat value (repeat \0)))))
+  (let [[mor mand] mask]
+    (assoc-in mem [:mem sink] (-> value
+                                  (bit-or mor)
+                                  (bit-and mand)))))
 
-(defonce docking-program
-  (read-program (slurp "resources/docking_program.txt")))
-
-(defn- read-binary [v]
-  (when (seq v)
-    (read-string (string/join (concat "2r" (reverse v))))))
+(defonce docking-program (slurp "resources/docking_program.txt"))
 
 (defn day14
-  ([version] (day14 version docking-program))
+  ([version] (day14 version (read-program version docking-program)))
   ([version instr]
-   (transduce
-    (map read-binary)
-    +
-    (-> execute-program
-        (reduce ,,, {:version version} instr)
-        :mem
-        vals))))
+   (reduce +
+           (-> (fn [m i] (execute-program m i))
+               (reduce ,,, {:version version} instr)
+               :mem
+               vals))))
 
 (def day14-1 (partial day14 1))
+
+(defmethod read-instruction [2 "mem"] [_ instr] (read-assign instr))
+
+(defn- generate-masks [msk]
+  (let [mor (mask msk {\X \0 \1 \1 \0 \0})
+        mand (mask msk {\X \0 \1 \1 \0 \1})]
+    (loop [[m & ms :as msk] msk
+           res [["2r0"]]]
+      (cond
+        ;; Convert all masks to longs
+        (empty? msk) [mor mand (mapv (fn [r] (read-string (string/join r)))
+                                     res)]
+        ;; Floating bit
+        (= m \X) (recur ms
+                        (concat (mapv #(conj % \0) res)
+                                (mapv #(conj % \1) res)))
+        :else (recur ms (mapv #(conj % m) res))))))
+
+(defmethod read-instruction [2 "mask"] [_ msk]
+  [:mask (generate-masks (drop 7 msk))])
 
 (defmethod execute-program [2 :mask] [mem [op new-mask]]
   (assoc mem op new-mask))
 
-(defn- generate-sinks [sink mask]
-  (let [sink (reverse (Integer/toBinaryString sink))]
-    (loop [[m & ms :as mask] mask
-           [curr & postfix] (concat sink (repeat 0))
-           res [[]]]
-      (cond
-        (empty? mask) (mapv read-binary res)
-        (= m \X) (recur ms
-                        postfix
-                        (concat (mapv #(conj % \0) res)
-                                (mapv #(conj % \1) res)))
-        (= m \0) (recur ms postfix (mapv #(conj % curr) res))
-        (= m \1) (recur ms postfix (mapv #(conj % 1) res))))))
-
 (defmethod execute-program [2 :assign] [{:keys [mask] :as mem} [_ sink value]]
-  (let [sinks (generate-sinks sink mask)]
-    (reduce (fn [m s] (assoc-in m [:mem s] value))
-            mem sinks)))
+  (let [[mor mand floating-bits] mask
+        sink (-> sink (bit-and mand) (bit-or mor))]
+    (transduce
+     (map (fn [f-bits] (bit-or sink f-bits)))
+     (fn
+       ([m] m)
+       ([m s] (assoc-in m [:mem s] value)))
+     mem floating-bits)))
 
 (def day14-2 (partial day14 2))
--- test/advent_of_code_2020/day14_test.clj
+++ test/advent_of_code_2020/day14_test.clj
@@ -12,46 +12,47 @@ mem[8] = 0")
 
 (deftest day14-1-example-1-read-programm-test
   (testing "Day 14 - Part 1 - Example 1 - read-program"
-    (is (= [[:mask (reverse "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")]
-            [:assign 8 (reverse "1011")]
-            [:assign 7 (reverse "1100101")]
-            [:assign 8 (reverse "0")]]
-           (#'advent-of-code-2020.day14/read-program example-input)))))
+    (is (= [[:mask [64 68719476733]]
+            [:assign 8 11]
+            [:assign 7 101]
+            [:assign 8 0]]
+           (into []
+                 (#'advent-of-code-2020.day14/read-program 1 example-input))))))
 
 (deftest day14-1-example-1-test
   (testing "Day 14 - Part 1 - Example 1"
-    (is (= 165 (day14-1
-                (#'advent-of-code-2020.day14/read-program example-input))))))
+    (is (= 165 (day14-1 (#'advent-of-code-2020.day14/read-program 1
+                                                                  example-input))))))
 
 (deftest day14-1-test
   (testing "Day 14 - Part 1"
     (is (= 14839536808842 (day14-1)))))
 
-(deftest day14-2-generate-sinks-test
-  (testing "Day 14 - Part 2 - Generate sinks - empty"
-    (is (= [nil] (#'advent-of-code-2020.day14/generate-sinks 0 [])))))
+(deftest day14-2-generate-masks-test
+  (testing "Day 14 - Part 2 - Generate masks - empty"
+    (is (= [0 0 [0]] (#'advent-of-code-2020.day14/generate-masks [])))))
 
-(deftest day14-2-generate-sinks-2-test
-  (testing "Day 14 - Part 2 - Generate sinks - [X]"
-    (is (= [0 1]
-           (#'advent-of-code-2020.day14/generate-sinks 0 [\X])))))
+(deftest day14-2-generate-masks-2-test
+  (testing "Day 14 - Part 2 - Generate masks - [X]"
+    (is (= [0 0 [0 1]]
+           (#'advent-of-code-2020.day14/generate-masks [\X])))))
 
-(deftest day14-2-generate-sinks-3-test
-  (testing "Day 14 - Part 2 - Generate sinks - [XX]"
-    (is (= [0 1 2 3]
-           (#'advent-of-code-2020.day14/generate-sinks 0 [\X \X])))))
+(deftest day14-2-generate-masks-3-test
+  (testing "Day 14 - Part 2 - Generate masks - [XX]"
+    (is (= [0 0 [0 2 1 3]]
+           (#'advent-of-code-2020.day14/generate-masks [\X \X])))))
 
-(deftest day14-2-generate-sinks-4-test
-  (testing "Day 14 - Part 2 - Generate sinks - [0]"
-    (is (= [1] (#'advent-of-code-2020.day14/generate-sinks 1 [\0])))))
+(deftest day14-2-generate-masks-4-test
+  (testing "Day 14 - Part 2 - Generate masks - [0]"
+    (is (= [0 1 [0]] (#'advent-of-code-2020.day14/generate-masks [\0])))))
 
-(deftest day14-2-generate-sinks-5-test
-  (testing "Day 14 - Part 2 - Generate sinks - [1]"
-    (is (= [1] (#'advent-of-code-2020.day14/generate-sinks 0 [\1])))))
+(deftest day14-2-generate-masks-5-test
+  (testing "Day 14 - Part 2 - Generate masks - [1]"
+    (is (= [1 1 [1]] (#'advent-of-code-2020.day14/generate-masks [\1])))))
 
-(deftest day14-2-generate-sinks-6-test
-  (testing "Day 14 - Part 2 - Generate sinks - [1 X]"
-    (is (= [1 3] (#'advent-of-code-2020.day14/generate-sinks 2 [\1 \X])))))
+(deftest day14-2-generate-masks-6-test
+  (testing "Day 14 - Part 2 - Generate masks - [1 X]"
+    (is (= [2 2 [2 3]] (#'advent-of-code-2020.day14/generate-masks [\1 \X])))))
 
 (defonce ^:private example-input-2
   "mask = 000000000000000000000000000000X1001X
@@ -62,8 +63,8 @@ mem[26] = 1")
 (deftest day14-2-example-2-test
   (testing "Day 14 - Part 2 - Example 2"
     (is (= 208
-           (day14-2
-            (#'advent-of-code-2020.day14/read-program example-input-2))))))
+           (day14-2 (#'advent-of-code-2020.day14/read-program 2
+                                                              example-input-2))))))
 
 (deftest day14-2-test
   (testing "Day 14 - Part 2"
