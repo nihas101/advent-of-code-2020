ef026ce3c0a2cc89b62328483310d53945f428f6
Nikita Hasert <nikitahasert@gmail.com>
2020-12-18 09:46:07 +0100

    Solve day 17

---
+++ src/advent_of_code_2020/day17.clj
@@ -0,0 +1,67 @@
+(ns advent-of-code-2020.day17
+  (:require
+   [clojure.string :as string]))
+
+(defonce ^:private cubes #{})
+
+(defn- read-cube-line [^long dims cubes [y line]]
+  (reduce (fn [c [x a]] (if (= \# a) (conj c (reduce conj [x y] (repeat (- dims 2) 0))) c))
+          cubes (mapv vector (range) line)))
+
+(defn- read-input [dims i]
+  (reduce (partial read-cube-line dims)
+          cubes
+          (mapv vector (range) (string/split-lines i))))
+
+(defn- expand-to-cube [p]
+  (loop [[d & ds :as dims] (range (count p))
+         ps [p]]
+    (if (seq dims)
+      (recur ds
+             (concat ps
+                     (mapv (fn [p] (update p d - 1)) ps)
+                     (mapv (fn [p] (update p d + 1)) ps)))
+      ps)))
+
+(defn- considerable-pos [cubes]
+  (distinct (mapcat expand-to-cube cubes)))
+
+(defn- active-neighbours [cubes pos]
+  (filter (partial contains? cubes)
+          ;; Remove the original position
+          (rest (expand-to-cube pos))))
+
+(defn- change-state [old-cubes new-cubes cube]
+  (let [active (contains? old-cubes cube)]
+    (cond
+      ;; The cube stays active
+      (and active (contains? #{2 3} (count (active-neighbours old-cubes cube))))
+      new-cubes
+      ;; An inactive cube becomes active
+      (and (not active) (= 3 (count (active-neighbours old-cubes cube))))
+      (conj new-cubes cube)
+      ;; Otherwise the cube becomes inactive
+      :else (disj new-cubes cube))))
+
+(defn- game-of-cubes-cycle [new-cubes]
+  (reduce (partial change-state new-cubes) new-cubes
+          (considerable-pos new-cubes)))
+
+(defonce ^:private init-cubes ".###..#.
+##.##...
+....#.#.
+#..#.###
+...#...#
+##.#...#
+#..##.##
+#.......")
+
+(defn- day17-fn [dims cycles]
+  (fn day17
+    ([] (day17 (read-input dims init-cubes) cycles))
+    ([init-cubes cycles]
+     (count (nth (iterate game-of-cubes-cycle init-cubes) cycles)))))
+
+(def day17-1 (day17-fn 3 6))
+
+(def day17-2 (day17-fn 4 6))
---
+++ test/advent_of_code_2020/day17_test.clj
@@ -0,0 +1,61 @@
+(ns advent-of-code-2020.day17-test
+  (:require
+   [clojure.test :refer :all]
+   [advent-of-code-2020.day17 :refer :all]))
+
+(defonce ^:private example-input
+  ".#.
+..#
+###")
+
+(defn- sort-pos [pos]
+  (sort-by (fn [p] (reduce (fn [^long r ^long d] (+ (* 10 r) d))
+                           (reverse p)))
+           pos))
+
+(deftest day17-1-example-1-read-input-test
+  (testing "Day 17 - Part 1 - Example 1 - read-input"
+    (is (= #{[1 0 0]
+             [2 1 0]
+             [0 2 0] [1 2 0] [2 2 0]}
+           (#'advent-of-code-2020.day17/read-input 3 example-input)))))
+
+(deftest day17-1-example-1-active-neighbours-test
+  (testing "Day 17 - Part 1 - Example 1 - active-neighbours"
+    (is (= (sort-pos [[2 1 0] [0 2 0] ,,, [2 2 0]])
+           (sort-pos
+            (#'advent-of-code-2020.day17/active-neighbours
+             (#'advent-of-code-2020.day17/read-input 3 example-input)
+             [1 2 0]))))))
+
+(deftest day17-1-example-1-change-state-active-test
+  (testing "Day 17 - Part 1 - Example 1 - change-state - active"
+    (let [c (#'advent-of-code-2020.day17/read-input 3 example-input)]
+      (is (= #{[1 0 0]
+               [2 1 0]
+               [0 2 0] [1 2 0] [2 2 0]}
+             (#'advent-of-code-2020.day17/change-state c c [1 2 0]))))))
+
+(deftest day17-1-example-1-change-state-inactive-test
+  (testing "Day 17 - Part 1 - Example 1 - change-state - inactive"
+    (let [c (#'advent-of-code-2020.day17/read-input 3 example-input)]
+      (is (= #{[2 1 0] [0 2 0] [1 2 0] [2 2 0]}
+             (#'advent-of-code-2020.day17/change-state c c [1 0 0]))))))
+
+(deftest day17-1-example-1-test
+  (testing "Day 17 - Part 1 - Example 1"
+    (let [c (#'advent-of-code-2020.day17/read-input 3 example-input)]
+      (is (= 11 (day17-1 c 1))))))
+
+(deftest day17-1-example-6-test
+  (testing "Day 17 - Part 1 - Example 1"
+    (let [c (#'advent-of-code-2020.day17/read-input 3 example-input)]
+      (is (= 112 (day17-1 c 6))))))
+
+(deftest day17-1-test
+  (testing "Day 17 - Part 1"
+    (is (= 304 (day17-1)))))
+
+(deftest day17-2-test
+  (testing "Day 17 - Part 2"
+    (is (= 1868 (day17-2)))))
