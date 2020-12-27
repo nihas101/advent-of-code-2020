2908292b9b4e8ec60c533380b27b87c8273c3028
Nikita Hasert <nikitahasert@gmail.com>
2020-12-27 19:14:43 +0100

    Solve day 20 part 2
    The solution is not ideal, but I have had my fill.

--- src/advent_of_code_2020/day20.clj
+++ src/advent_of_code_2020/day20.clj
@@ -12,13 +12,14 @@
     (-> (reduce (fn [im [y image-row]]
                   (reduce (fn [im [x pixel]]
                             (if (= \# pixel)
-                              (assoc im [x y] 1)
+                              (assoc! im [x y] 1)
                               im))
                           im (mapv vector (range) image-row)))
-                {}
+                (transient {})
                 (mapv vector (range) image))
-        (assoc ,,, :width width)
-        (assoc ,,, :height height))))
+        (assoc! ,,, :width width)
+        (assoc! ,,, :height height)
+        persistent!)))
 
 (defn- read-tile [tile]
   (let [[tile-id & image] (string/split-lines tile)]
@@ -28,11 +29,35 @@
 (defn- read-input [s]
   (mapv read-tile (u/split-sections s)))
 
-(defn- serialize-edge [edge]
-  [(read-string (string/join (conj edge "2r"))) ; Ordinary value
-   (read-string (string/join (conj (reverse edge) "2r")))]) ; Flipped value
+(defn- serialize-border [border]
+  [(read-string (string/join (conj border "2r"))) ; Ordinary value
+   (read-string (string/join (conj (reverse border) "2r")))]) ; Flipped value
 
-(defn- serialize-edges [{:keys [width height] :as image}]
+(defn- remove-border! [{:keys [^long width ^long height] :as image}]
+  (reduce (fn [i [x y]] (dissoc! i [x y]))
+          image
+          (concat (mapv vector (range width) (repeat 0)) ; North
+                  (mapv vector (repeat (dec width)) (range height)) ; East
+                  (mapv vector (range width) (repeat (dec height))) ; South
+                  (mapv vector (repeat 0) (range height))))) ; West
+
+(defn- fix-coords [{:keys [width height] :as content}]
+  (persistent!
+   (reduce (fn [c [^long x ^long y]]
+             (assoc! c [(dec x) (dec y)] 1))
+           (transient {:width width :height height})
+           (remove #{:width :height} (keys content)))))
+
+(defn- extract-content [{:keys [^long width ^long height] :as image}]
+  (-> image
+      transient
+      remove-border!
+      (assoc! ,,, :width (- width 2))
+      (assoc! ,,, :height (- height 2))
+      persistent!
+      fix-coords))
+
+(defn- serialize-image [{:keys [^long width ^long height] :as image}]
   (let [north (map (fn [x y] (get image [x y] 0))
                    (range width) (repeat 0))
         east (map (fn [x y] (get image [x y] 0))
@@ -41,49 +66,214 @@
                    (range width) (repeat (dec height)))
         west (map (fn [x y] (get image [x y] 0))
                   (repeat 0) (range height))]
-    {:north (serialize-edge north)
-     :east (serialize-edge east)
-     :south (serialize-edge south)
-     :west (serialize-edge west)
+    {:north (serialize-border north)
+     :east (serialize-border east)
+     :south (serialize-border south)
+     :west (serialize-border west)
+     :content (extract-content image)
      :flipped false}))
 
-(defn- rotate-edges [edges]
-  (let [old-north (:north edges)]
-    (-> edges
-        (assoc ,,, :north (:west edges))
-        (assoc ,,, :west (:south edges))
-        (assoc ,,, :south (:east edges))
-        (assoc ,,, :east old-north))))
+(defn- flip-border [{:keys [north east south west flipped] :as tile}]
+  (-> tile
+      transient
+      (assoc! ,,, :north south)
+      (assoc! ,,, :south north)
+      (assoc! ,,, :east (reverse east))
+      (assoc! ,,, :west (reverse west))
+      ;; Remember that we flipped the border
+      (assoc! ,,, :flipped (not flipped))
+      persistent!))
 
-(defn- flip-edges [edges]
-  (update edges :flipped not))
+(defn- rotate-border [{:keys [north west south east rotation] :as tile}]
+  (-> tile
+      transient
+      (assoc! ,,, :north east)
+      (assoc! ,,, :east (reverse south))
+      (assoc! ,,, :south west)
+      (assoc! ,,, :west (reverse north))
+      ;; Remember the rotation to rotate the content later
+      (assoc! ,,, :rotation (if rotation (+ ^long rotation 90) 90))
+      persistent!))
 
-(defn- get-edges [{:keys [flipped] :as edges}]
-  (let [es ((juxt :north :east :south :west) edges)]
-    (if flipped (mapv second es) (mapv first es))))
+(defn- possible-transforms [tile]
+  (let [rots (take 4 (iterate #(update % :image rotate-border) tile))]
+    (concat rots (mapv #(update % :image flip-border) rots))))
 
-(defn- collect-edges [image-tiles]
-  (mapcat get-edges
-          (concat (mapv :image image-tiles)
-                  (mapv #(flip-edges (:image %)) image-tiles))))
+(defn- get-border-values [tile]
+  (let [bs ((juxt :north :east :south :west) tile)]
+    (mapv first bs)))
+
+(defn- collect-borders [image-tiles]
+  (eduction
+   (comp (mapcat possible-transforms)
+         (map :image)
+         (mapcat get-border-values))
+   image-tiles))
 
 (defn- find-corners [image-tiles]
   (loop [[corner-candidate & corner-candidates] image-tiles
          corners []]
-    (let [other-edges (collect-edges (remove #{corner-candidate} image-tiles))]
+    (let [other-borders (collect-borders (remove #{corner-candidate} image-tiles))]
       (cond
         ;; We are done
         (nil? corner-candidate) corners
         ;; We found a corner piece
-        (= 2 (count (remove (set other-edges) (get-edges (:image corner-candidate)))))
+        (= 2 (count (remove (set other-borders) (get-border-values (:image corner-candidate)))))
         (recur corner-candidates (conj corners corner-candidate))
         ;; We found a tile thats not a corner piece
         :else (recur corner-candidates corners)))))
 
+(defonce ^:private image-tiles
+  (read-input (slurp "resources/image_tiles.txt")))
+
 (defn day20-1
-  ([] (day20-1 (read-input (slurp "resources/image_tiles.txt"))))
+  ([] (day20-1 image-tiles))
   ([image-tiles]
    (transduce
     (map :tile-id)
     *
-    (find-corners (mapv #(update % :image serialize-edges) image-tiles)))))
+    (find-corners (mapv #(update % :image serialize-image) image-tiles)))))
+
+(defn- get-border [border tile]
+  (-> tile border first))
+
+(defonce ^:private opposite
+  {:north :south
+   :east :west
+   :south :north
+   :west :east})
+
+(defn- possible-neighbors [tile border tiles]
+  (let [opp (opposite border)
+        b (-> tile :image ((partial get-border border)))]
+    (transduce
+     (comp (remove (fn [{:keys [tile-id]}] (= (:tile-id tile) tile-id)))
+           (mapcat possible-transforms)
+           (filter (fn [{:keys [image]}] (= b (get-border opp image)))))
+     conj
+     tiles)))
+
+(defn- top-left-tiles [tiles]
+  (eduction
+   (comp (mapcat possible-transforms)
+         (filter #(and (seq (possible-neighbors % :east tiles))
+                       (seq (possible-neighbors % :south tiles)))))
+   (find-corners tiles)))
+
+(defn- rotate
+  ([[^double x0 ^double y0] [^long x ^long y] ^long deg]
+   (let [[^double xx ^double yy] (rotate [(- x x0) (- y y0)] deg)]
+     [(long (+ xx x0)) (long (+ yy y0))]))
+  ([[^double x y] ^long deg]
+   (if (pos? deg)
+     (recur [y (- x)] (- deg 90))
+     [x y])))
+
+(defn- rotate-content [{:keys [image] :as image-tile}]
+  (let [{:keys [rotation content]} image
+        width (:width content)
+        height (:height content)]
+    (if rotation
+      (assoc-in image-tile [:image :content]
+                (persistent!
+                 (reduce (fn [c [p]]
+                           (assoc! c (rotate [(/ (dec ^long width) 2) (/ (dec ^long height) 2)] p rotation) 1))
+                         (transient {:width width :height height})
+                         (dissoc content :width :height))))
+      image-tile)))
+
+(defn- flip [[x ^long y] ^long height]
+  [x (- (dec height) y)])
+
+(defn- flip-content [{:keys [image] :as image-tile}]
+  (let [{:keys [flipped content]} image
+        width (:width content)
+        height (:height content)]
+    (if flipped
+      (assoc-in image-tile [:image :content]
+                (persistent!
+                 (reduce (fn [c [p]] (assoc! c (flip p height) 1))
+                         (transient {:width width :height height})
+                         (dissoc content :width :height))))
+      image-tile)))
+
+(defn- adjust-content [image-row]
+  (eduction
+   (map (fn [image] (flip-content (rotate-content image))))
+   image-row))
+
+(defn- image-tile->row-strings [{:keys [image]}]
+  (let [content (:content image)
+        width (-> content :width)
+        height (-> content :height)]
+    (mapv string/join
+          (for [y (range height)]
+            (for [x (range width)]
+              (if (get content [x y]) "#" "."))))))
+
+(defn- stitch-row-together [image-row]
+  (string/join "\n"
+               (apply mapv str
+                      (mapv image-tile->row-strings image-row))))
+
+(defn- stitch-image-together [image-rows]
+  (string/join "\n" (mapv stitch-row-together image-rows)))
+
+(defn- connect-strip [tile direction image-tiles]
+  (loop [tile tile
+         [neighbor] (possible-neighbors tile direction image-tiles)
+         strip [tile]]
+    (if neighbor
+      ;; We found a tile that fits in this direction
+      (recur neighbor
+             (possible-neighbors neighbor direction image-tiles)
+             (conj strip neighbor))
+      ;; There are no more tiles that fit, return the strip
+      strip)))
+
+(defn- sea-monster [spacing]
+  (re-pattern
+   (str
+    ;; We use the dotall mode (?s), so '.' also matches newline characters
+    "(?s)"
+    ;; We put the space between the monster in matching groups in order
+    ;; to only replace the monsters body and keep the space between
+    "(..................)#(..{" spacing "})"
+    "#(....)##(....)##(....)###(.{" spacing "}"
+    ".)#(..)#(..)#(..)#(..)#(..)#")))
+
+(defonce ^:private mark-sea-monster
+  ;; We keep the space we put into matching groups the same and mark the monster
+  ;; with O characters
+  "$1O$2O$3OO$4OO$5OOO$6O$7O$8O$9O$10O$11O")
+
+(defn- build-image [image-tiles from]
+  (persistent!
+   (transduce
+    (comp (map (fn [edge-tile] (connect-strip edge-tile :east image-tiles)))
+          (map adjust-content))
+    conj!
+    ;; We first form the west edge and then build the image from there
+    (connect-strip from :south image-tiles))))
+
+(defn- fix-replace [s match replacement]
+  ;; Replace the match with a replacement until no match is found anymore
+  (let [n (string/replace s match replacement)]
+    (if (= n s) n (recur n match replacement))))
+
+(defn day20-2
+  ([] (day20-2 image-tiles 77))
+  ([image-tiles spacing]
+   (let [image-tiles (mapv (fn [it] (update it :image serialize-image))
+                           image-tiles)]
+     (transduce
+      (comp (map stitch-image-together)
+            (map (fn [im] (fix-replace im (sea-monster spacing) mark-sea-monster)))
+            (map #(filter #{\#} %))
+            (map count))
+      min
+      Long/MAX_VALUE
+      ;; Build the 8 flipped and rotated images in parallel, one of which is the
+      ;; correct image. This is not ideal, but better than nothing
+      (pmap (partial build-image image-tiles)
+            (top-left-tiles image-tiles))))))
--- test/advent_of_code_2020/day20_test.clj
+++ test/advent_of_code_2020/day20_test.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-2020.day20-test
   (:require
+   [clojure.string :as string]
    [clojure.test :refer :all]
    [advent-of-code-2020.day20 :refer :all]))
 
@@ -230,8 +231,36 @@ Tile 3079:
              :tile-id 3079}]
            (#'advent-of-code-2020.day20/read-input example-input)))))
 
-(deftest day20-example-serialize-edges-test
-  (testing "Day 20 - Part 1 - serialize-edges"
+(deftest day20-example-serialize-image-only-border-test
+  (testing "Day 20 - Part 1 - serialize-image only border"
+    (let [im "Tile 1:\n#####\n#...#\n#...#\n#...#\n#####"]
+      (is (= {:north [31 31]
+              :east [31 31]
+              :south [31 31]
+              :west [31 31]
+              :content {:height 3 :width 3}
+              :flipped false}
+             (#'advent-of-code-2020.day20/serialize-image
+              (-> (#'advent-of-code-2020.day20/read-tile im) :image)))))))
+
+(deftest day20-example-serialize-image-no-border-test
+  (testing "Day 20 - Part 1 - serialize-image no border"
+    (let [im "Tile 1:\n.....\n.###.\n.###.\n.###.\n....."]
+      (is (= {:north [0 0]
+              :east [0 0]
+              :south [0 0]
+              :west [0 0]
+              :content {:height 3
+                        :width 3
+                        [0 0] 1 [0 1] 1 [0 2] 1
+                        [1 0] 1 [1 1] 1 [1 2] 1
+                        [2 0] 1 [2 1] 1 [2 2] 1}
+              :flipped false}
+             (#'advent-of-code-2020.day20/serialize-image
+              (-> (#'advent-of-code-2020.day20/read-tile im) :image)))))))
+
+(deftest day20-example-serialize-image-test
+  (testing "Day 20 - Part 1 - serialize-image"
     (let [im {:height 10
               :width 10
               [0 1] 1 [0 2] 1 [0 3] 1 [0 4] 1 [0 5] 1 [0 8] 1
@@ -247,22 +276,33 @@ Tile 3079:
               :east [89 616]
               :south [231 924]
               :west [498 318]
+              :content {:height 8
+                        :width 8
+                        [0 0] 1 [0 2] 1 [0 3] 1 [0 4] 1 [0 5] 1 [0 7] 1
+                        [1 2] 1 [1 6] 1 [1 7] 1
+                        [2 2] 1 [2 3] 1 [2 5] 1
+                        [3 0] 1 [3 1] 1 [3 3] 1
+                        [4 1] 1 [4 2] 1 [4 4] 1 [4 5] 1
+                        [5 3] 1 [5 7] 1
+                        [6 3] 1 [6 4] 1 [6 6] 1
+                        [7 1] 1 [7 3] 1 [7 4] 1 [7 5] 1 [7 7] 1}
               :flipped false}
-             (#'advent-of-code-2020.day20/serialize-edges im))))))
+             (#'advent-of-code-2020.day20/serialize-image im))))))
 
-(deftest day20-example-rotate-edges-test
-  (testing "Day 20 - Part 1 - rotate-edges"
+(deftest day20-example-rotate-border-test
+  (testing "Day 20 - Part 1 - rotate-border"
     (let [edges {:north [210 300]
                  :east [89 616]
                  :south [231 924]
                  :west [498 318]
                  :flipped false}]
-      (is (= {:north [498 318]
-              :east [210 300]
-              :south [89 616]
-              :west [231 924]
-              :flipped false}
-             (#'advent-of-code-2020.day20/rotate-edges edges))))))
+      (is (= {:east [924 231]
+              :flipped false
+              :north [89 616]
+              :rotation 90
+              :south [498 318]
+              :west [300 210]}
+             (#'advent-of-code-2020.day20/rotate-border edges))))))
 
 (deftest day20-example-flip-edges-test
   (testing "Day 20 - Part 1 - flip-edges"
@@ -271,24 +311,24 @@ Tile 3079:
                  :south [231 924]
                  :west [498 318]
                  :flipped false}]
-      (is (= {:north [210 300]
-              :east [89 616]
-              :south [231 924]
-              :west [498 318]
-              :flipped true}
-             (#'advent-of-code-2020.day20/flip-edges edges))))))
+      (is (= {:east [616 89]
+              :flipped true
+              :north [231 924]
+              :south [210 300]
+              :west [318 498]}
+             (#'advent-of-code-2020.day20/flip-border edges))))))
 
-(deftest day20-example-get-edges-test
-  (testing "Day 20 - Part 1 - get-edges"
-    (let [edges {:north [210 300]
-                 :east [89 616]
-                 :south [231 924]
-                 :west [498 318]
-                 :flipped false}]
+(deftest day20-example-get-borders-test
+  (testing "Day 20 - Part 1 - get-borders"
+    (let [borders {:north [210 300]
+                   :east [89 616]
+                   :south [231 924]
+                   :west [498 318]
+                   :flipped false}]
       (are [x y] (= x y)
-        [210 89 231 498] (#'advent-of-code-2020.day20/get-edges edges)
-        [300 616 924 318] (#'advent-of-code-2020.day20/get-edges
-                           (assoc edges :flipped true))))))
+        [210 89 231 498] (#'advent-of-code-2020.day20/get-border-values borders)
+        [231 616 210 318] (#'advent-of-code-2020.day20/get-border-values
+                           (#'advent-of-code-2020.day20/flip-border borders))))))
 
 (deftest day20-1-example-test
   (testing "Day 20 - Part 1 - example"
@@ -299,3 +339,252 @@ Tile 3079:
 (deftest day20-1-test
   (testing "Day 20 - Part 1"
     (is (= 79412832860579 (day20-1)))))
+
+(deftest day20-example-transforms-test
+  (testing "Day 20 - Part 2 - possible-transforms"
+    (let [im "Tile 1:\n.####\n.....\n#...#\n#...#\n#.###"]
+      (is (= [{:image {:content {:height 3 :width 3}
+                       :east [23 29]
+                       :flipped false
+                       :north [15 30]
+                       :south [23 29]
+                       :west [7 28]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [29 23]
+                       :flipped false
+                       :north [23 29]
+                       :rotation 90
+                       :south [7 28]
+                       :west [30 15]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [28 7]
+                       :flipped false
+                       :north [29 23]
+                       :rotation 180
+                       :south [30 15]
+                       :west [29 23]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [15 30]
+                       :flipped false
+                       :north [28 7]
+                       :rotation 270
+                       :south [29 23]
+                       :west [23 29]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [29 23]
+                       :flipped true
+                       :north [23 29]
+                       :south [15 30]
+                       :west [28 7]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [23 29]
+                       :flipped true
+                       :north [7 28]
+                       :rotation 90
+                       :south [23 29]
+                       :west [15 30]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [7 28]
+                       :flipped true
+                       :north [30 15]
+                       :rotation 180
+                       :south [29 23]
+                       :west [23 29]}
+               :tile-id 1}
+              {:image {:content {:height 3 :width 3}
+                       :east [30 15]
+                       :flipped true
+                       :north [29 23]
+                       :rotation 270
+                       :south [28 7]
+                       :west [29 23]}
+               :tile-id 1}]
+             (#'advent-of-code-2020.day20/possible-transforms
+              (update (#'advent-of-code-2020.day20/read-tile im)
+                      :image #'advent-of-code-2020.day20/serialize-image)))))))
+
+(deftest day20-2-rotate-around-origin-test
+  (testing "Day 20 - Part 2 - rotate around origin"
+    (are [x y] (= x (mapv long y))
+      [0 -1] (#'advent-of-code-2020.day20/rotate [1  0] 90)
+      [1  0] (#'advent-of-code-2020.day20/rotate [0  1] 90)
+      [0  1] (#'advent-of-code-2020.day20/rotate [-1 0] 90)
+      [-1 0] (#'advent-of-code-2020.day20/rotate [0 -1] 90)
+
+      [-1 0] (#'advent-of-code-2020.day20/rotate [1  0] 180)
+      [0 -1] (#'advent-of-code-2020.day20/rotate [0  1] 180)
+      [1  0] (#'advent-of-code-2020.day20/rotate [-1 0] 180)
+      [0  1] (#'advent-of-code-2020.day20/rotate [0 -1] 180)
+
+      [0  1] (#'advent-of-code-2020.day20/rotate [1  0] 270)
+      [-1 0] (#'advent-of-code-2020.day20/rotate [0  1] 270)
+      [0 -1] (#'advent-of-code-2020.day20/rotate [-1 0] 270)
+      [1  0] (#'advent-of-code-2020.day20/rotate [0 -1] 270))))
+
+(deftest day20-2-rotate-test
+  (testing "Day 20 - Part 2 - rotate"
+    (are [x y] (= x y)
+      [1 -1] (#'advent-of-code-2020.day20/rotate [1 0] [2  0] 90)
+      [2  0] (#'advent-of-code-2020.day20/rotate [1 0] [1  1] 90)
+      [1  1] (#'advent-of-code-2020.day20/rotate [1 0] [0  0] 90)
+      [0  0] (#'advent-of-code-2020.day20/rotate [1 0] [1 -1] 90)
+      [0  8] (#'advent-of-code-2020.day20/rotate [4 4] [0  0] 90)
+      [8  8] (#'advent-of-code-2020.day20/rotate [4 4] [0  0] 180)
+      [8  0] (#'advent-of-code-2020.day20/rotate [4 4] [0  0] 270)
+      [8  8] (#'advent-of-code-2020.day20/rotate [4 4] [0  0] 180))))
+
+(deftest day20-2-flip-test
+  (testing "Day 20 - Part 2 - flip"
+    (are [x y] (= x y)
+      [1 9] (#'advent-of-code-2020.day20/flip [1 0] 10)
+      [0 8] (#'advent-of-code-2020.day20/flip [0 1] 10)
+      [9 9] (#'advent-of-code-2020.day20/flip [9 0] 10)
+      [0 0] (#'advent-of-code-2020.day20/flip [0 9] 10))))
+
+(defonce ^:private test-image-tiles
+  "Tile 1:
+#.#.#
+.#..#
+#.#.#
+...##
+...##
+
+Tile 2:
+....#
+#..#.
+#.#..
+##...
+###..
+
+Tile 3:
+...##
+...##
+..#.#
+.#..#
+#...#
+
+Tile 4:
+###..
+##...
+#.#..
+#..#.
+#...#")
+
+(deftest day20-2-test-image-tiles-test
+  (testing "Day 20 - Part 2 - test-image-tiles"
+    (let [tile (update
+                (first (#'advent-of-code-2020.day20/read-input test-image-tiles))
+                :image #'advent-of-code-2020.day20/serialize-image)]
+      (is (= "#..\n.#.\n..#"
+             (->> tile
+                  (#'advent-of-code-2020.day20/rotate-content)
+                  (#'advent-of-code-2020.day20/flip-content)
+                  (#'advent-of-code-2020.day20/image-tile->row-strings)
+                  (string/join "\n")))))))
+
+(deftest day20-2-test-image-tiles-flipped-test
+  (testing "Day 20 - Part 2 - test-image-tiles"
+    (let [tile (-> (first (#'advent-of-code-2020.day20/read-input test-image-tiles))
+                   (update ,,, :image #'advent-of-code-2020.day20/serialize-image)
+                   (update ,,, :image #'advent-of-code-2020.day20/flip-border))]
+      (is (= "..#\n.#.\n#.."
+             (->> tile
+                  (#'advent-of-code-2020.day20/rotate-content)
+                  (#'advent-of-code-2020.day20/flip-content)
+                  (#'advent-of-code-2020.day20/image-tile->row-strings)
+                  (string/join "\n")))))))
+
+(deftest day20-2-test-image-tiles-rotation-test
+  (testing "Day 20 - Part 2 - test-image-tiles"
+    (let [tile (-> (first (#'advent-of-code-2020.day20/read-input test-image-tiles))
+                   (update ,,, :image #'advent-of-code-2020.day20/serialize-image)
+                   (update ,,, :image #'advent-of-code-2020.day20/rotate-border))]
+      (is (= "..#\n.#.\n#.."
+             (->> tile
+                  (#'advent-of-code-2020.day20/rotate-content)
+                  (#'advent-of-code-2020.day20/flip-content)
+                  (#'advent-of-code-2020.day20/image-tile->row-strings)
+                  (string/join "\n")))))))
+
+(deftest day20-2-test-image-tiles-flipped-rotation-test
+  (testing "Day 20 - Part 2 - test-image-tiles"
+    (let [tile (-> (first (#'advent-of-code-2020.day20/read-input test-image-tiles))
+                   (update ,,, :image #'advent-of-code-2020.day20/serialize-image)
+                   (update ,,, :image #'advent-of-code-2020.day20/rotate-border)
+                   (update ,,, :image #'advent-of-code-2020.day20/flip-border))]
+      (is (= "#..\n.#.\n..#"
+             (->> tile
+                  (#'advent-of-code-2020.day20/rotate-content)
+                  (#'advent-of-code-2020.day20/flip-content)
+                  (#'advent-of-code-2020.day20/image-tile->row-strings)
+                  (string/join "\n")))))))
+
+(deftest day20-2-test-image-test
+  (testing "Day 20 - Part 2 - test-image-tiles"
+    (let [tiles (partition 2 2
+                           (mapv
+                            #(update % :image #'advent-of-code-2020.day20/serialize-image)
+                            (#'advent-of-code-2020.day20/read-input test-image-tiles)))]
+      (is (= "#....#\n.#..#.\n..##..\n..##..\n.#..#.\n#....#"
+             (#'advent-of-code-2020.day20/stitch-image-together tiles))))))
+
+
+(deftest day20-2-regex-test
+  (testing "Day 20 - Part 2 - regex"
+    (is (= (str "..................O.\n"
+                "O....OO....OO....OOO\n"
+                ".O..O..O..O..O..O...")
+           (#'advent-of-code-2020.day20/fix-replace
+            (str "..................#.\n"
+                 "#....##....##....###\n"
+                 ".#..#..#..#..#..#...")
+            (#'advent-of-code-2020.day20/sea-monster 1)
+            "$1O$2O$3OO$4OO$5OOO$6O$7O$8O$9O$10O$11O")))))
+
+(deftest day20-2-regex-border-test
+  (testing "Day 20 - Part 2 - regex"
+    (is (= (str "xxxxxxxxxxxxxxxxxxxxxx\n"
+                "x..................O.x\n"
+                "xO....OO....OO....OOOx\n"
+                "x.O..O..O..O..O..O...x\n"
+                "xxxxxxxxxxxxxxxxxxxxxx")
+           (#'advent-of-code-2020.day20/fix-replace
+            (str "xxxxxxxxxxxxxxxxxxxxxx\n"
+                 "x..................#.x\n"
+                 "x#....##....##....###x\n"
+                 "x.#..#..#..#..#..#...x\n"
+                 "xxxxxxxxxxxxxxxxxxxxxx")
+            (#'advent-of-code-2020.day20/sea-monster 3)
+            "$1O$2O$3OO$4OO$5OOO$6O$7O$8O$9O$10O$11O")))))
+
+(deftest day20-2-regex-offset-test
+  (testing "Day 20 - Part 2 - regex"
+    (is (= (str "xxxxxxxxxxxxxxxxxxxxxx\n"
+                "..................O.xx\n"
+                "O....OO....OO....OOOxx\n"
+                ".O..O..O..O..O..O...xx\n"
+                "xxxxxxxxxxxxxxxxxxxxxx")
+           (#'advent-of-code-2020.day20/fix-replace
+            (str "xxxxxxxxxxxxxxxxxxxxxx\n"
+                 "..................#.xx\n"
+                 "#....##....##....###xx\n"
+                 ".#..#..#..#..#..#...xx\n"
+                 "xxxxxxxxxxxxxxxxxxxxxx")
+            (#'advent-of-code-2020.day20/sea-monster 3)
+            "$1O$2O$3OO$4OO$5OOO$6O$7O$8O$9O$10O$11O")))))
+
+(deftest day20-2-example-test
+  (testing "Day 20 - Part 2 - example"
+    (is (= 273 (day20-2
+                (#'advent-of-code-2020.day20/read-input example-input)
+                5)))))
+
+(deftest day20-2-test
+  (testing "Day 20 - Part 2 - example"
+    (is (= 2155 (day20-2)))))
